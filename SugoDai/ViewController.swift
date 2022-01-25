141873866f784b52c32d17c9b5816835d9edcc26
Kokona Kato <koko72219@yahoo.co.jp>
2022-01-25 23:37:12 +0900

    Initial Commit

---
+++ SugoDai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SugoDai
+//
+//  Created by 加藤 on 2022/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
