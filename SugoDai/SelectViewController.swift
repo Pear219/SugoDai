9a48e5c1b889cae7ad9bfe3f2582160318029c38
Kokona Kato <koko72219@yahoo.co.jp>
2022-01-26 00:29:07 +0900

    選択画面、タイマー(オブジェクトだけ)設置

---
+++ SugoDai/SelectViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SelectViewController.swift
+//  SugoDai
+//
+//  Created by 加藤 on 2022/01/25.
+//
+
+import UIKit
+
+class SelectViewController: UIViewController {
+    
+    @IBOutlet var Button1: UIButton!
+    @IBOutlet var Button2: UIButton!
+    @IBOutlet var Button3: UIButton!
+    @IBOutlet var Button4: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
