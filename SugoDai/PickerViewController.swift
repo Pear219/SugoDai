57b8ea7a6a45c4e982b5f762c9f0c5253ad72d4e
Kokona Kato <koko72219@yahoo.co.jp>
2022-01-26 18:43:41 +0900

    色々追加

---
+++ SugoDai/PickerViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PickerViewController.swift
+//  SugoDai
+//
+//  Created by 加藤 on 2022/01/26.
+//
+
+import UIKit
+
+class PickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
