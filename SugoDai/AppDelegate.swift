db925f912c51e8fd571e2b6a548b4e1f80971830
Kokona Kato <koko72219@yahoo.co.jp>
2023-05-09 20:42:51 +0900

    charts

--- SugoDai/AppDelegate.swift
+++ SugoDai/AppDelegate.swift
@@ -10,8 +10,11 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    
 
+    
+    
+    
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
